b8bc511b51feb423f6301e9dd6af05d250d1ec90
Amir mohammad <am.sanei79@gmail.com>
2022-02-17 04:07:35 +0330

    Add the NavBar component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import About from "./pages/about";
 import "./App.css";
 import Home from './pages/home';
 import NotFound from "./pages/notFound";
+import NavBar from "./compenents/NavBar";
 
 function App() {
   return (
     <Router>
+      <NavBar />
       <Routes>
         <Route path="/about" element={<About />} />
         <Route path="/" element={<Home />} />
