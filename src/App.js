ad73c0ae4ec3b93e771dd67e2d8f211357219f4e
Amir mohammad <am.sanei79@gmail.com>
2022-02-17 04:20:25 +0330

    Add param to profile page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./pages/about";
 import "./App.css";
-import Home from './pages/home';
+import Home from "./pages/home";
 import NotFound from "./pages/notFound";
 import NavBar from "./compenents/NavBar";
-import Profile from './pages/profile';
+import Profile from "./pages/profile";
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/about" element={<About />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/profile/:username" element={<Profile />} />
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,9 +1,15 @@
 import React from "react";
-import {useNavigate} from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom";
 
 function Profile() {
-    let navigate = useNavigate();
-  return (<div>this is Profile page <button onClick={() => navigate("/about")}>About page</button> </div>);
+  let navigate = useNavigate();
+  let { username } = useParams();
+  return (
+    <div>
+      this is Profile page for {username}{" "}
+      <button onClick={() => navigate("/about")}>About page</button>{" "}
+    </div>
+  );
 }
 
 export default Profile;
